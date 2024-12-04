4cf2e6d46c0e9b99744ced8279ac5908553bb625
Sohum Berry <sohumberry@gmail.com>
2024-12-03 20:34:51 -0800

    First Log

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
-Name: YOUR NAME HERE
+Name: Sohum Berry
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |    Time     |                                                                                                                                                                                                           Update |
+|:--------|:-----------:|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| 12/3/24 | 11:15-11:55 | I sort of mapped out what I'm planning to do, then I started to try to do it. It is really not looking great so far, everything is really messy, I think I'm going to try to get the words a little differently. |
+|         |             |                                                                                                                                                                                                                  |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -21,6 +21,13 @@
  *  = 43.54% compression ratio!
  ******************************************************************************/
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *  The {@code TextCompressor} class provides static methods for compressing
  *  and expanding natural language through textfile input.
@@ -28,12 +35,70 @@
  *  @author Zach Blick, YOUR NAME HERE
  */
 public class TextCompressor {
+    private ArrayList<String> words;
 
-    private static void compress() {
+    public TextCompressor() {
+        this.words = new ArrayList<>();
+    }
+
+    private static void compress() throws IOException {
+        /**
+         * ok here's the plan:
+         * z = number of bits that will be used for the codes
+         *Compression:
+         * 1. Go through the input string and search for the most common substrings in it
+         *         First I'm going to check for most common of a certain length like 3 or 4
+         *         Then later I can make it search for the lengths from 2 to like 10
+         *         and calculate the top x of them by multiplying their frequency by their length
+         * 2. Assign the top z-2 of the frequent words to a z length bit in a map, then write it as a header (saving this for later, gonna get later steps to work first
+         * 3. Start writing each character by character with 8 bits, leave something like 1111 1111 as an
+         *      escape character that will be written when a substring that is in the map is detected
+         * 4. When finished writing out a code, write an escape character like 1111111111
+         *
+         * Expansion:
+         * 1. Read in the map into an object
+         * 2. Start with assuming it's a real word until you hit escape character, then write out codes based on the map
+         */
+
+        Scanner s = new Scanner(new File("common.txt"));
+        s.useDelimiter("\\n");
+        String commonWordsString = "";
+        while (s.hasNext()) {
+            commonWordsString += s.next();
+            commonWordsString += "\n";
+        }
+        String[] commonWords = commonWordsString.split("\n");
+        System.out.println(commonWords.length);
+
+        String input = BinaryStdIn.readString();
+        makeWord(input, "");
 
         // TODO: Complete the compress() method
 
         BinaryStdOut.close();
+    }
+
+    private static int getSubstringCode(String[] commonWords, String word) {
+        // wow this is gonna be slow
+        for (int i = 0; i < commonWords.length; i++) {
+            if (commonWords[i].equals(word)) {
+                return i;
+            }
+        }
+        return commonWords.length;
+    }
+
+    // From my SpellingBee code
+    // Recursively generating every combination of the given letters
+    private static void makeWord(String input, String creation) throws IOException {
+        for (int i = 0; i < (input.isEmpty() ? 1 : input.length()); i++) {
+            FileWriter myWriter = new FileWriter("words.txt");
+            if (!creation.isEmpty())
+                myWriter.write(creation);
+            if (input.isEmpty()) return;
+            myWriter.close();
+            makeWord(input.substring(0, i) + input.substring(i+1), creation + input.charAt(i));
+        }
     }
 
     private static void expand() {
@@ -43,7 +108,7 @@ public class TextCompressor {
         BinaryStdOut.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if      (args[0].equals("-")) compress();
         else if (args[0].equals("+")) expand();
         else throw new IllegalArgumentException("Illegal command line argument");
